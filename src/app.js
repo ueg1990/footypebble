7ab488d4e2f29241263a5856f3d4dca153c9cc48
Usman Ehtesham Gul <uehtesham90@gmail.com>
2015-02-22 12:06:29 -0500

    Version 1

--- src/app.js
+++ src/app.js
@@ -35,6 +35,11 @@ var main = new UI.Menu({
 main.show();
 
 var score_board;
+var time_frame_start;
+var time_frame_stop;
+//var LEAGUE_CODES = ['BL1', 'PL', 'SA', 'PD','FL1','CL', 'EC'];
+var LEAGUE_CODES = ['BL1', 'PL', 'SA', 'PD', 'CL'];
+var URL_fixtures = 'http://api.football-data.org/alpha/fixtures/';
 
 main.on('select', function(e) {
   var card = new UI.Card();
@@ -42,17 +47,19 @@ main.on('select', function(e) {
   if (e.item.title === 'Update') {
     splashCard.show();
     setTimeout(function() {
-  }, 5000);
-    var URL = 'http://api.football-data.org/alpha/fixtures/';
+}, 5000);
     ajax(
   {
-    url: URL,
+    url: URL_fixtures,
     type: 'json'
   },
   function(data) {
-    // Success!
-    score_board = data;
-    card.subtitle("Successfully fetched data!: " + data.length);
+    // Success!aaqq22222
+    score_board = data.fixtures;
+    time_frame_start = data.timeFrameStart; 
+    time_frame_stop = data.timeFrameEnd;
+    //card.subtitle("Successfully fetched data!: " + data.timeFrameStart);
+    //card.show();
     main.show();
     splashCard.hide();
   },
@@ -65,9 +72,72 @@ main.on('select', function(e) {
   
   }
   else {
-    card.title('A Card');
-    card.subtitle('Is a Window');
-    card.body('Selected item #' + e.item.title + ' of section #' + e.section.title + " : " + score_board);
+    card.title('Scorecard');
+    //card.body(time_frame_start + ", " + time_frame_stop);
+    var score_body = getCurrentDate() + '\n';
+    var league_results = {};
+    for (var fixture = 0; fixture < 20 ; fixture++) {
+      //console.log("URL: " + score_board[fixture]._links.soccerseason.href);
+      //var league = getLeague(score_board[fixture]._links.soccerseason.href);
+      //console.log("League: " + league);
+//       if (LEAGUE_CODES.indexOf(league[0]) >= 0) {
+//         league_results[league[0]].push({'home_team' : score_board[fixture].homeTeamName , 
+//                                         'away_team': score_board[fixture].awayTeamName,
+//                                         'goals_home_team': score_board[fixture].result.goalsHomeTeam , 
+//                                         'goals_away_team': score_board[fixture].result.goalsAwayTeam});
+//       }
+      //console.log("before score");
+       score_body += score_board[fixture].homeTeamName +" ("+score_board[fixture].result.goalsHomeTeam+") - ("+
+        score_board[fixture].result.goalsAwayTeam + ") "+ score_board[fixture].awayTeamName +"\n";
+      score_body += "" + fixture;
+      //console.log("after score");
+    }
+    //score_body += 'Team scores!!!'; //leage_results.length
+    card.body(score_body);
     card.show();
   }
 });
+
+function getCurrentDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+  var yyyy = today.getFullYear();
+  
+  if(dd<10) {
+      dd='0'+dd;
+  } 
+  
+  if(mm<10) {
+      mm='0'+mm;
+  } 
+  
+  today = mm+'-'+dd+'-'+yyyy;
+  return today;
+}
+function getLeague(fixture) {
+  var result;
+  ajax(
+  {
+    url: fixture,
+    type: 'json'
+  },
+  function(data) {
+    // Success!aaqq22222
+    //console.log(data);
+    result = [data.league, data.caption];
+    //return get_result(result);
+    //console.log(result);
+    
+  },
+  function(error) {
+    // Failure!
+    console.log("Failure!!!");
+    //return null;
+    //card.subtitle('Failed fetching weather data: ' + error);
+    //card.show();
+  }
+);
+  //console.log(result);
+   //return result;
+}
