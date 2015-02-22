8390394337293780e3557f963e426e3237b94494
Usman Ehtesham Gul <uehtesham90@gmail.com>
2015-02-21 22:02:41 -0500

    Structure partially defined

--- src/app.js
+++ src/app.js
@@ -5,54 +5,69 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
+var ajax = require('ajax');
+//var Vector2 = require('vector2');
 
-var main = new UI.Card({
-  title: 'Pebble.js',
-  icon: 'images/menu_icon.png',
-  subtitle: 'Hello World!',
-  body: 'Press any button.'
+// var main = new UI.Card({
+//   title: 'Pebble.js',
+//   icon: 'images/menu_icon.png',
+//   subtitle: 'Hello World!',
+//   body: 'Press any button.'
+// });
+
+// Show splash
+var splashCard = new UI.Card({
+  title: "Please Wait",
+  body: "Downloading..."
+});
+
+var main = new UI.Menu({
+  sections: [{
+    title: 'Options',
+    items: [{
+      title: 'Update',
+    }, {
+      title: 'Scores'
+    }]
+  }]
 });
 
 main.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+var score_board;
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window();
-  var textfield = new UI.Text({
-    position: new Vector2(0, 50),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
+main.on('select', function(e) {
   var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
+  card.scrollable(true);
+  if (e.item.title === 'Update') {
+    splashCard.show();
+    setTimeout(function() {
+  }, 5000);
+    var URL = 'http://api.football-data.org/alpha/fixtures/';
+    ajax(
+  {
+    url: URL,
+    type: 'json'
+  },
+  function(data) {
+    // Success!
+    score_board = data;
+    card.subtitle("Successfully fetched data!: " + data.length);
+    main.show();
+    splashCard.hide();
+  },
+  function(error) {
+    // Failure!
+    card.subtitle('Failed fetching weather data: ' + error);
+    card.show();
+  }
+);
+  
+  }
+  else {
+    card.title('A Card');
+    card.subtitle('Is a Window');
+    card.body('Selected item #' + e.item.title + ' of section #' + e.section.title + " : " + score_board);
+    card.show();
+  }
 });
